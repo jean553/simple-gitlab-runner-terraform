0a4db2bd09155eff530e4c48cba940dad68c8419
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-05-08 18:17:15 +0200

    ensure instance access

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,8 @@
 variable "access_key" {}
 variable "secret_key" {}
 variable "region" {}
+variable "key" {}
+variable "pem_file" {}
 variable "simple-gitlab-runner-ami" {}
 
 provider "aws" {
@@ -12,11 +14,32 @@ provider "aws" {
 resource "aws_instance" "simple-gitlab-runner" {
   ami                        = "${var.simple-gitlab-runner-ami}"
   instance_type              = "t2.micro"
+  key_name                   = "${var.key}"
   associate_public_ip_address= true
+  security_groups            = ["${aws_security_group.allow-all-inbound-ssh.name}"]
   tags {
     Name                     = "simple-gitlab-runner"
   }
-  provisioner "local-exec" {
-    command                  = "./firstboot.sh"
+  provisioner "remote-exec" {
+    inline                   = [
+      "sudo chmod +x /firstboot.sh",
+      "sudo /firstboot.sh"
+    ]
+
+    connection {
+      user                   = "admin"
+      private_key            = "${file(var.pem_file)}"
+    }
+  }
+}
+
+resource "aws_security_group" "allow-all-inbound-ssh" {
+  name                       = "allow-all-inbound-ssh"
+  description                = "allow only inbound SSH traffic"
+  ingress {
+    from_port                = 22
+    to_port                  = 22
+    protocol                 = "tcp"
+    cidr_blocks              = ["0.0.0.0/0"]
   }
 }
